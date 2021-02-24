eff86179175283bd7ec26b08e7a133e73223b806
Danica <dnc.stjnvc@gmail.com>
2021-02-24 11:32:54 -0800

    Setup with dependencies completed (#1)

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('client/dist'));
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
