0367d82c4a557a659348c7e834742052d3486164
Zachary Shold <sholdzb@gmail.com>
2021-02-25 14:17:33 -0600

    Jest and rudimentary tests successfully implemented

---
+++ spec/tests.spec.js
@@ -0,0 +1,19 @@
+describe('Test function', () => {
+  const testFunc = () => {
+    return 1;
+  };
+
+  test('test function should be defined', () => {
+
+    expect(testFunc).toBeDefined();
+  })
+
+
+  test('test function should return 1', () => {
+
+    const output = 1;
+
+    expect(testFunc()).toEqual(output);
+  });
+
+});
