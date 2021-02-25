94220776db0fdf49256f29242edbcd3a76735a9d
danicastojanovic <dnc.stjnvc@gmail.com>
2021-02-25 10:13:40 -0800

    updated linter

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('client/dist'));
+app.use(express.static('client'));
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
