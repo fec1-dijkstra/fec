a2e138dd6da81ab187808405d078dc7513f2ac48
Mikl Barton <miklbarton@gmail.com>
2021-03-01 10:42:19 -0700

    convert Overview component from functional to stateful

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const Overview = () => <div>This is the overview component!</div>;
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <div>This is the overview component!</div>
+      </div>
+    );
+  }
+}
 export default Overview;
