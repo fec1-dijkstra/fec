b3c7e81471dfeff2a3b4af991541d3560d10c249
Zachary Shold <sholdzb@gmail.com>
2021-03-01 12:54:45 -0600

    implements more reviews button that renders 2 reviews per press

--- client/src/components/ReviewTile/ReviewTile.jsx
+++ client/src/components/ReviewTile/ReviewTile.jsx
@@ -1,15 +1,24 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 
 class ReviewTile extends React.Component {
   constructor(props) {
     super();
-    this.state = { reviews: props.reviews, rendered: props.reviews.slice(0, 2) };
+    this.state = { reviews: props.reviews, total: 2 };
+    this.moreReviews = this.moreReviews.bind(this);
+  }
+
+  moreReviews(event) {
+    event.preventDefault();
+    debugger;
+    const count = this.state.total;
+    this.setState({ total: count + 2 });
   }
 
   render() {
-    console.log(this.state);
-    const toRender = this.state.rendered.map((review) => (
-      <div className="review-tile">
+    // console.log(this.state);
+    const toRender = this.state.reviews.slice(0, this.state.total).map((review) => (
+      <div className="review-tile" key={review.name}>
         <div className="star-rating">{review.rating}</div>
         <div className="date">Month DD, YYYY</div>
         <div className="username">{review.name}</div>
@@ -18,7 +27,14 @@ class ReviewTile extends React.Component {
         <div className="review-body">{review.body}</div>
       </div>
     ));
-    return <div className="reviews">{toRender}</div>;
+    return (
+      <div className="reviews">
+        {toRender}{' '}
+        <button type="submit" id="more-reviews" onClick={this.moreReviews}>
+          More Reviews
+        </button>
+      </div>
+    );
   }
 }
 
