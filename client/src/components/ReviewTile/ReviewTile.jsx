c58a722b6a48ce5bd666fa494dc1e01bbe778a81
Zachary Shold <sholdzb@gmail.com>
2021-03-01 13:33:40 -0600

    implements accurate timestamps on review tiles

--- client/src/components/ReviewTile/ReviewTile.jsx
+++ client/src/components/ReviewTile/ReviewTile.jsx
@@ -10,31 +10,34 @@ class ReviewTile extends React.Component {
 
   moreReviews(event) {
     event.preventDefault();
-    debugger;
     const count = this.state.total;
     this.setState({ total: count + 2 });
   }
 
   render() {
     // console.log(this.state);
+    const totalReviews = this.state.reviews.length;
     const toRender = this.state.reviews.slice(0, this.state.total).map((review) => (
       <div className="review-tile" key={review.name}>
         <div className="star-rating">{review.rating}</div>
-        <div className="date">Month DD, YYYY</div>
+        <div className="date">{new Date(review.date).toString().substring(0, 15)}</div>
         <div className="username">{review.name}</div>
         <div className="summary">{review.summary}</div>
         <div className="summary-ext">...rest of summary</div>
         <div className="review-body">{review.body}</div>
       </div>
     ));
-    return (
-      <div className="reviews">
-        {toRender}{' '}
-        <button type="submit" id="more-reviews" onClick={this.moreReviews}>
-          More Reviews
-        </button>
-      </div>
-    );
+    if (totalReviews > this.state.total) {
+      return (
+        <div className="reviews">
+          {toRender}
+          <button type="submit" id="more-reviews" onClick={this.moreReviews}>
+            More Reviews
+          </button>
+        </div>
+      );
+    }
+    return <div className="reviews">{toRender}</div>;
   }
 }
 
