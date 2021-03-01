643121c3045058aa294fd08f9726b688f5064ce0
Christopher Tavolazzi <ctavolazzi@gmail.com>
2021-03-01 11:18:05 -0800

    Add folder for the QandA component (#15)

---
+++ client/src/components/QandA/QandA.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class QandA extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>QandA component goes here!</div>;
+  }
+}
+
+export default QandA;
