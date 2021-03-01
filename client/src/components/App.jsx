ae313e2bf2c75a4cf5afb82a726b38013091e05f
Mikl Barton <miklbarton@gmail.com>
2021-03-01 13:23:54 -0700

    created queries.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-// eslint-disable-next-line import/extensions
+    // eslint-disable-next-line import/extensions
 import ReviewTile from './ReviewTile/ReviewTile.jsx';
 import Overview from './Overview/Overview.jsx';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
 // eslint-disable-next-line import/extensions
 import products from '../../dummydata.js';
+import queries from './queries.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = { items: products };
+  }
+
+  componentDidMount() {
+    queries.getProductList(1, 1, (result) => {
+      console.log(result);
+    });
   }
 
   render() {
---
+++ client/src/components/queries.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const auth = require('../../../token.js');
+
+// EXAMPLE CALL
+// import queries from '../queries.js';
+// queries.getProductList(1, 1, (result) => {
+//   console.log(result);
+// });
+
+module.exports.queries = {
+
+  getProductList: (pageNumber, countNumber, cb) => {
+    const page = `page=${pageNumber}`;
+    const count = `count=${countNumber}`;
+
+    const productList = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products?${page}&${count}`,
+      headers: {
+        Authorization: auth.myToken,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    axios(productList)
+      .then((response) => {
+        cb(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
