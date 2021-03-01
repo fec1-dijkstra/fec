ac3214c88b55f3c8d7a1058482aa6a1558e6c11d
danicastojanovic <dnc.stjnvc@gmail.com>
2021-03-01 10:09:10 -0800

    created environment for my component

---
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RelatedItems extends React.Components {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return();
+  }
+}
+
+export default RelatedItems;
