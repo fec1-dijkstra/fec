c6954b2a09b88a6ecaf8499b1cedb2c73e61a2c4
Christopher Tavolazzi <ctavolazzi@gmail.com>
2021-03-02 11:50:15 -0800

    Hard coded the scaffolding for the question card.

--- client/src/components/QandA/QCard.jsx
+++ client/src/components/QandA/QCard.jsx
@@ -5,11 +5,26 @@ class QCard extends React.Component {
     super(props);
     this.state = {
       data: []
-    }
+    };
   }
 
   render() {
-    return <div>QCard</div>;
+    return (
+      <div>
+        QCard
+        <div>Q: Some question?</div>
+        <div>
+          A:
+          <div>Some answer.</div>
+          <div>Some other answer.</div>
+        </div>
+        <div>
+          by Username, Date
+          <div>Helpful? Yes (1)</div>
+          <div>Report</div>
+        </div>
+      </div>
+    )
   }
 }
 
