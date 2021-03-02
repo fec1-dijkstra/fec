db1b536b9e5dc85b4e5124b85cbb26b5d97af72b
Mikl Barton <miklbarton@gmail.com>
2021-03-01 17:20:30 -0700

    cleanup in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,8 @@
+/* eslint-disable import/extensions */
 import React from 'react';
-    // eslint-disable-next-line import/extensions
 import ReviewTile from './ReviewTile/ReviewTile.jsx';
 import Overview from './Overview/Overview.jsx';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
-// eslint-disable-next-line import/extensions
 import products from '../../dummydata.js';
 import queries from './queries.js';
 
