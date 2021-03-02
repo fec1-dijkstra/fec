de165af7448f5cd6c36ab247d6cf8df558bd0ef8
danicastojanovic <dnc.stjnvc@gmail.com>
2021-03-01 16:53:42 -0800

    modal open/close

--- client/src/components/Relatedandoutfit/Modal.jsx
+++ client/src/components/Relatedandoutfit/Modal.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
 
-const Modal = function () {
+const Modal = ({ showModal, handleCloseModal }) => {
+  const showHideClassName = showModal ? 'modal display-block' : 'modal display-none';
   return (
-  <div className="modalContent">
-  {/* /products/productId and /products/relatedProductId .features.forEach
-  render a <tr> component, if feature already exists, add cell with value */}
-      <h3>COMPARING</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Current Product Name</th>
-            <th></th>
-            <th>Compared Product Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>Green Leaf Certified</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Cashmere</td>
-            <td>Fabric</td>
-            <td>100% Cotton</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>Lifetime Guarantee</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>Satisfaction Guaranteed</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td>Non-GMO</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-  </div>
+    <div className={showHideClassName}>
+      {/* /products/productId and /products/relatedProductId .features.forEach
+    render a <tr> component, if feature already exists, add cell with value */}
+      <section className="modal-main">
+        <h3>COMPARING</h3>
+        <button type="button" onClick={handleCloseModal}>
+          Close
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>Current Product Name</th>
+              <th></th>
+              <th>Compared Product Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>Green Leaf Certified</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Cashmere</td>
+              <td>Fabric</td>
+              <td>100% Cotton</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Lifetime Guarantee</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Satisfaction Guaranteed</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Non-GMO</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 };
 
--- client/src/components/Relatedandoutfit/ProductCard.jsx
+++ client/src/components/Relatedandoutfit/ProductCard.jsx
@@ -1,40 +1,59 @@
 import React from 'react';
 import Modal from './Modal.jsx';
 
-const ProductCard = function () {
-  return (
-    <div className="ProductCard">
-      <button type="button" id="modalButton">★</button>
-      {/* pass product features and related product features down to modal */}
-      <Modal className="modal" />
-      <img
-        src="https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1511&q=80"
-        alt="DefaultStyleImage"
-        className="ProductCardImage"
-      />
-      <div className="ProductInfo">
-        {/* From /products/productId .category */}
-        <p className="Category">Tank Top</p>
-        {/* From /products/productId .name - /products/productId/styles .results[0].name */}
-        <p className="ProductName">Forrest Tank Top - Magenta</p>
-        <p className="ProductPrice">
-          {/* products/:id/styles
-          if (.results[0].sale_price) */}
-          <span className="SalePrice">$201.00</span>
-          <span className="OriginalPrice">$253.00</span>
-          {/* else if .results[0].sale_price === null
-            <span>$253.00</span> */}
-        </p>
-        {/* calculate star rating from reviews/meta?product_id=PRODUCT_ID
-        for (var key in .ratings) {
-          total+=obj.key*key (toNumber);
-          ratings+=obj.key
-        }
-        average=total/ratings */}
-        <p className="Stars">★★★☆☆</p>
+class ProductCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+    };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
+
+  render() {
+    return (
+      <div className="ProductCard">
+        <button type="button" onClick={this.handleOpenModal} id="modalButton">★</button>
+        {/* pass product features and related product features down to modal */}
+        <Modal className="modal" showModal={this.state.showModal} handleCloseModal={this.handleCloseModal} />
+        <img
+          src="https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1511&q=80"
+          alt="DefaultStyleImage"
+          className="ProductCardImage"
+        />
+        <div className="ProductInfo">
+          {/* From /products/productId .category */}
+          <p className="Category">Tank Top</p>
+          {/* From /products/productId .name - /products/productId/styles .results[0].name */}
+          <p className="ProductName">Forrest Tank Top - Magenta</p>
+          <p className="ProductPrice">
+            {/* products/:id/styles
+            if (.results[0].sale_price) */}
+            <span className="SalePrice">$201.00</span>
+            <span className="OriginalPrice">$253.00</span>
+            {/* else if .results[0].sale_price === null
+              <span>$253.00</span> */}
+          </p>
+          {/* calculate star rating from reviews/meta?product_id=PRODUCT_ID
+          for (var key in .ratings) {
+            total+=obj.key*key (toNumber);
+            ratings+=obj.key
+          }
+          average=total/ratings */}
+          <p className="Stars">★★★☆☆</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProductCard;
