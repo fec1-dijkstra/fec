4f6e864795dc6f04e35334acd6f8c3ab7e3ed3a8
Zachary Shold <sholdzb@gmail.com>
2021-03-02 09:17:14 -0600

    Mostly finished (unstyled) review tiles component (#20)
    * Jest and rudimentary tests successfully implemented
    
    * adds jest client dependencies
    
    * implements enzyme tests, moves jest standalone tests to same directory
    
    * adds eslint dependencies
    
    * reverts back to eslint commit (526060c)
    
    * generates current package-lock.json
    
    * adds dummydata.js and products
    
    * reinstalled dependencies
    
    * formats several files to abide by style guide
    
    * adds qa data for first product
    
    * finished implementing base app component and ReviewTile Folder/component
    
    * additional comment, changed app.jsx to be component agnostic
    
    * passes dummydata from App to ReviewTile
    
    * passes reviews properly down to ReviewTile
    
    * implements more reviews button that renders 2 reviews per press
    
    * implements accurate timestamps on review tiles
    
    * renames ReviewTiles to RenderReviews, refacotred to tow components
    
    * username now properly displays on reviews
    
    * implements summary component
    
    * reformats testComponent file
    
    * implements tests for Summary Component
    
    * implements ReviewBody Component that dynamically renders read more button
    
    * refactors RenderReviews and some other lines to read cleaner
    
    * implements checkmarks on product recommendations
    
    * implements early version of helpfulness counter
    
    * implements helpful button (only clickable once by user)
    
    * helpful button now dissapears upon click
    
    Co-authored-by: Mikl Barton <miklbarton@gmail.com>

---
+++ client/src/components/tests/SummaryTest.test.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Summary from '../ReviewTiles/Summary.jsx';
+
+configure({
+  adapter: new Adapter(),
+});
+
+describe('Summary tests', () => {
+  const text =
+    'This is a summary with a length that exceeds the summary limit of sixty characters, and should be divided between two divs';
+  const summ = shallow(<Summary summary={text} />);
+
+  it('renders a summary', () => {
+    expect(summ.find('.summary')).toBeDefined();
+  });
+  it('should split the summary across two lines', () => {
+    expect(summ.find('.summary-cont')).toBeDefined();
+  });
+});
