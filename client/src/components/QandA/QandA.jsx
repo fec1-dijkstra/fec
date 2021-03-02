15a5ee44598a6bd86fc3a0e746a3746197b04c76
Christopher Tavolazzi <ctavolazzi@gmail.com>
2021-03-01 17:49:48 -0800

    Scaffold the QandA component

--- client/src/components/QandA/QandA.jsx
+++ client/src/components/QandA/QandA.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class QandA extends React.component {
+class QandA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -9,7 +9,14 @@ class QandA extends React.component {
   componentDidMount() {}
 
   render() {
-    return <div>QandA component goes here!</div>;
+    return (
+      <div>
+        QandA component goes here!
+        <div>Nested Inside main QandA: The QASearch Component</div>
+        <div>Nested Inside main QandA: The QuestionList Component</div>
+        <div>Nested Inside main QandA: The AddAQuestion Component</div>
+      </div>
+    );
   }
 }
 
