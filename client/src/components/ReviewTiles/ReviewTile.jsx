2bfbcdd2b835a12d29a5ee781604e8d6e4621b47
Zachary Shold <sholdzb@gmail.com>
2021-03-03 15:23:58 -0600

    Adds Image Modal to Raitings and Reviews as well as stylesheet (#26)
    * Jest and rudimentary tests successfully implemented
    
    * adds jest client dependencies
    
    * implements enzyme tests, moves jest standalone tests to same directory
    
    * adds eslint dependencies
    
    * reverts back to eslint commit (526060c)
    
    * generates current package-lock.json
    
    * adds dummydata.js and products
    
    * reinstalled dependencies
    
    * formats several files to abide by style guide
    
    * adds qa data for first product
    
    * finished implementing base app component and ReviewTile Folder/component
    
    * additional comment, changed app.jsx to be component agnostic
    
    * passes dummydata from App to ReviewTile
    
    * passes reviews properly down to ReviewTile
    
    * implements more reviews button that renders 2 reviews per press
    
    * implements accurate timestamps on review tiles
    
    * renames ReviewTiles to RenderReviews, refacotred to tow components
    
    * username now properly displays on reviews
    
    * implements summary component
    
    * reformats testComponent file
    
    * implements tests for Summary Component
    
    * implements ReviewBody Component that dynamically renders read more button
    
    * refactors RenderReviews and some other lines to read cleaner
    
    * implements checkmarks on product recommendations
    
    * implements early version of helpfulness counter
    
    * implements helpful button (only clickable once by user)
    
    * helpful button now dissapears upon click
    
    * removes deprecated ReviewTile folder/files
    
    * implements review images with rudimentary click handler (does nothing useful yet)
    
    * refactors RenderPhoto to longer have native state or methods
    
    * images now render at full resolution on click
    
    * adds stylesheet
    
    * image modals now render in center of page at full resolution
    
    * makes review images slightly smaller (by 50px)
    
    * removes unused Modal component
    
    * implements framework for SortBy and helper functions/components
    
    Co-authored-by: Mikl Barton <miklbarton@gmail.com>

--- client/src/components/ReviewTiles/ReviewTile.jsx
+++ client/src/components/ReviewTiles/ReviewTile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
@@ -6,6 +7,8 @@ import Summary from './Summary.jsx';
 import ReviewBody from './ReviewBody.jsx';
 import Recommends from './Recommends.jsx';
 import Helpful from './Helpful.jsx';
+import ShowPhotos from './ShowPhotos.jsx';
+import Response from './Response.jsx';
 
 class ReviewTile extends React.Component {
   constructor() {
@@ -20,10 +23,12 @@ class ReviewTile extends React.Component {
         <div className="star-rating">{review.rating}</div>
         <div className="date">{new Date(review.date).toString().substring(3, 15)}</div>
         <div className="username">{review.reviewer_name}</div>
+        <Response response={review.response} />
         <Summary summary={review.summary} />
         <ReviewBody body={review.body} />
         <Recommends recommendation={review.recommend} />
         <Helpful helpfulness={review.helpfulness} />
+        <ShowPhotos photos={review.photos} />
       </div>
     );
   }
