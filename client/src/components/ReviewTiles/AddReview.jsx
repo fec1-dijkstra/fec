86dcc1533fee2782a351d967b8af03fa7951d0b8
Zachary Shold <sholdzb@gmail.com>
2021-03-03 16:22:20 -0600

    implements recommendation radio buttons in review modal

--- client/src/components/ReviewTiles/AddReview.jsx
+++ client/src/components/ReviewTiles/AddReview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class AddReview extends React.Component {
   constructor() {
     super();
-    this.state = { show: false };
+    this.state = { show: false, rec: null };
     this.showModal = this.showModal.bind(this);
   }
 
@@ -13,13 +13,26 @@ class AddReview extends React.Component {
     this.setState({ show: !show });
   }
 
+  select(event) {
+    const pick = event.target.value;
+    this.setState({ rec: pick });
+  }
+
   render() {
     const { show } = this.state;
     if (show) {
       return (
         <div className="modal-background">
           <div className="modal-box">
-            <div id="text">This is the review modal!</div>
+            <div id="write">Write your Review</div>
+            <div id="item">
+              About the <b>Placeholder</b> here
+            </div>
+            <div className="radio" onChange={this.select}>
+              Would you Recommend this Product?
+              <input type="radio" value="yes" name="recommend" /> Yes
+              <input type="radio" value="no" name="recommend" /> No
+            </div>
             <button type="submit" onClick={this.showModal}>
               Close
             </button>
