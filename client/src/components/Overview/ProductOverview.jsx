425e74aff5f318a77bb2d5229e398dc05c53d0ef
Mikl Barton <miklbarton@gmail.com>
2021-03-03 15:38:09 -0700

    product overview now renders nothing if not provided

--- client/src/components/Overview/ProductOverview.jsx
+++ client/src/components/Overview/ProductOverview.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Features from './Features.jsx';
 
-const ProductOverview = ({ productInfo }) => (
-  <div>
-    <div>{productInfo.slogan}</div>
-    <div>{productInfo.description}</div>
-    <Features productInfo={productInfo} />
-  </div>
-);
+const ProductOverview = ({ productInfo }) => {
+  const hasSlogan = () => {
+    if (productInfo.slogan) {
+      return <div>{productInfo.slogan}</div>;
+    }
+    return <></>;
+  };
+  const hasDescription = () => {
+    if (productInfo.description) {
+      return <div>{productInfo.description}</div>;
+    }
+    return <></>;
+  };
+  const hasFeatures = () => {
+    if (productInfo.features) {
+      return <Features productInfo={productInfo} />;
+    }
+    return <></>;
+  };
+  return (
+    <>
+      {hasSlogan()}
+      {hasDescription()}
+      {hasFeatures()}
+    </>
+  );
+};
 
 ProductOverview.defaultProps = {
   productInfo: {},
