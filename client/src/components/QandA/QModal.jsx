06567ec2f2fb57e3343b924c0064303afc44ad7c
Christopher Tavolazzi <ctavolazzi@gmail.com>
2021-03-03 17:11:54 -0800

    QandA refactor to allow proper passage of props (#32)
    * Moved the "Add an Answer" button inside the IndvQ component
    
    * Username and date render dynamically in the IndvA component
    
    * Fixed bug where "Add an Answer" button was showing up twice
    
    * Refactoring the AModal component to use a form
    
    * Renamed IndvQ to Question, IndvA to Answer
    
    * Pass data into QCard component
    
    * Refactored the search component to receive input
    
    * Refactored the question and answer modals to more closely match the business requirements docs
    
    * Render QList component. Refactor QSearch component to take input from user.
    
    * Moved the qa data from the main module to the QCard module
    
    * Refactor so props can be passed appropriately
    
    * Removed unnecessary imports

--- client/src/components/QandA/QModal.jsx
+++ client/src/components/QandA/QModal.jsx
@@ -3,18 +3,49 @@ import React from 'react';
 class QModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      textAreaValue: 'Please write your question here',
+      nickname: '',
+      email: '',
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleNicknameChange = this.handleNicknameChange.bind(this);
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleTextAreaChange(e) {
+    this.setState({ textAreaValue: e.target.value });
+  }
+
+  handleNicknameChange(e) {
+    this.setState({ nickname: e.target.value });
+  }
+
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  static handleSubmit(e) {
+    // TODO: handle form submission
+    // figure out why 'static' has to be here for the linter to not yell
+    e.preventDefault();
   }
 
   render() {
+    const { textAreaValue } = this.state;
+    const { nickname } = this.state;
+    const { email } = this.state;
     return (
-      <div>
-        QModal
-        <div>Your Question</div>
-        <div>Your Nickname</div>
-        <div>Your Email</div>
-        <button type="button">Submit</button>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="test">
+          Add a Question
+          <textarea value={textAreaValue} onChange={this.handleTextAreaChange} />
+          <input type="text" value={nickname} onChange={this.handleNicknameChange} />
+          <input type="text" value={email} onChange={this.handleEmailChange} />
+        </label>
+        <input type="submit" value="Submit" onClick={this.handleSubmit} />
+      </form>
     );
   }
 }
--- client/src/components/QandA/QandA.jsx
+++ client/src/components/QandA/QandA.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import QCard from './QCard.jsx';
 import QSearch from './QSearch.jsx';
+import QList from './QList.jsx';
 
-import { qa } from '../../../dummydata.js';
+// import { qa } from '../../../dummydata.js';
 
 class QandA extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: qa,
-    };
+    this.state = {};
   }
 
-  componentDidMount() {}
-
   render() {
-    const { data } = this.state;
+    // console.log('qa:', qa);
     return (
       <div>
         QandA component goes here!
         <div>----start----</div>
         <QSearch />
-        <QCard data={data} />
+        <QList />
         <button type="button">Add a Question</button>
         <div>-----end-----</div>
       </div>
