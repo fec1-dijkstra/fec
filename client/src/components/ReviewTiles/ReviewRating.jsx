85dec266d7180ffb2634788837c9493c683923fe
Zachary Shold <sholdzb@gmail.com>
2021-03-04 15:43:29 -0600

    can now click stars in AddReview to select a star rating

--- client/src/components/ReviewTiles/ReviewRating.jsx
+++ client/src/components/ReviewTiles/ReviewRating.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 
-const ReviewRating = ({ rating }) => {
-  const starCount = [1, 2, 3, 4, 5];
-  const starRating = starCount.map(() => {
-    if (rating) {
-      rating -= 1;
-      return <span className="checked">★</span>;
-    }
-    return <span className="unchecked">★</span>;
-  });
-  return <div className="rating">{starRating}</div>;
-};
+class ReviewRating extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rating: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const { id } = event.target;
+    debugger;
+    this.setState({ rating: id });
+  }
+
+  render() {
+    const starCount = [1, 2, 3, 4, 5];
+    let { rating } = this.state;
+    const starRating = starCount.map((ignore, index) => {
+      if (rating) {
+        rating -= 1;
+        return (
+          <span className="checked" id={`${index + 1}`} onClick={this.handleClick}>
+            ★
+          </span>
+        );
+      }
+      return (
+        <span className="unchecked" id={`${index + 1}`} onClick={this.handleClick}>
+          ★
+        </span>
+      );
+    });
+    return <div className="rating">{starRating}</div>;
+  }
+}
 
 export default ReviewRating;
