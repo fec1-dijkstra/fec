fc683e9d736d0a59128cdea0f1b13d5fc39a09d0
Zachary Shold <sholdzb@gmail.com>
2021-03-04 21:21:44 -0600

    validateSubmission now properly handles restructured characteristics

--- client/src/components/ReviewTiles/functions/validateSubmit.js
+++ client/src/components/ReviewTiles/functions/validateSubmit.js
@@ -21,7 +21,7 @@ const validateSubmit = (state, meta) => {
   let validation;
   if (state && meta) {
     keys.map((key) => {
-      if (!state[key] || state[key].length === 0) {
+      if (!state.characteristics[key] || state.characteristics[key].length === 0) {
         validation = `a selection for ${key} is required`;
         return null;
       }
