66998cf174a5c211e0e56bdf03bc33cf0cf2cac4
Mikl Barton <miklbarton@gmail.com>
2021-03-12 12:13:30 -0700

    expanded view arrow functionality

--- client/src/components/Overview/ExpandedView.jsx
+++ client/src/components/Overview/ExpandedView.jsx
@@ -7,26 +7,60 @@ const ExpandedView = ({
   zoomExpand,
   openExpand,
   expandedRef,
+  navClick,
 }) => {
   if (selectedStyle.photos && selectedStyle.photos.length > 0) {
+    let arrowLeft = <></>;
+    let arrowRight = <></>;
+    const leftArrow = (
+      <div
+        className="overview-arrow-background overview-expanded-arrow-background-left"
+        onClick={navClick}
+        role="button"
+        onKeyPress={navClick}
+        tabIndex={0}
+      >
+        <div className="overview-expanded-arrow" id="overview-arrow-left" />
+      </div>
+    );
+    const rightArrow = (
+      <div
+        className="overview-arrow-background overview-expanded-arrow-background-right"
+        onClick={navClick}
+        role="button"
+        onKeyPress={navClick}
+        tabIndex={0}
+      >
+        <div className="overview-expanded-arrow" id="overview-arrow-right" />
+      </div>
+    );
+    if (selectedThumbnail !== 0) {
+      arrowLeft = leftArrow;
+    }
+    if (selectedThumbnail < selectedStyle.photos.length - 1) {
+      arrowRight = rightArrow;
+    }
     return (
       <div className="overview-expanded-view-wrapper">
-        <div className="overview-fade-out" />
+        <div
+          className="overview-fade-out"
+          onClick={openExpand}
+          role="button"
+          tabIndex={0}
+          onKeyPress={zoomExpand}
+          aria-label="button"
+        />
         <div
           className="overview-expanded-view"
-          onBlur={openExpand}
+          // onBlur={openExpand}
           onClick={zoomExpand}
           onKeyPress={zoomExpand}
           tabIndex={0}
           role="button"
           ref={expandedRef}
         >
-          <div className="overview-arrow-background overview-expanded-arrow-background-left">
-            <div className="overview-expanded-arrow" />
-          </div>
-          <div className="overview-arrow-background overview-expanded-arrow-background-right">
-            <div className="overview-expanded-arrow" />
-          </div>
+          {arrowLeft}
+          {arrowRight}
           <img
             src={selectedStyle.photos[selectedThumbnail].url}
             alt={`${selectedStyle.name} default view`}
